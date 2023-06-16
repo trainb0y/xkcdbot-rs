fa46a62cbfb9d9daeffa5d3ac0df684ea7ca72da
trainb0y <trainb0ydev@gmail.com>
2023-06-16 09:20:48 -0600

    Comic struct

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,64 @@
+use datetime::{LocalDate, Month};
+use poise::futures_util::TryFutureExt;
 use poise::samples::register_in_guild;
 use poise::serenity_prelude::GuildId;
 use poise::{serenity_prelude as serenity, Command, FrameworkBuilder};
+use serde::Deserialize;
 
-#[derive(Copy, Clone)]
-struct Data {}
+struct Data {
+    client: reqwest::Client,
+}
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
+const EXPLAIN_URL: &str = "https://explainxkcd.com";
+const XKCD_URL: &str = "https://xkcd.com";
+
 // save me from myself
 fn commands() -> Vec<Command<Data, Error>> {
-    return vec![ping()];
+    vec![ping(), debug_get()]
+}
+
+#[derive(Deserialize)]
+struct Comic {
+    pub num: u32,
+    pub title: String,
+    pub alt: String,
+
+    // why is it like this?
+    // I don't know, ask Randall
+    day: String,
+    month: String,
+    year: String,
+}
+
+impl Comic {
+    fn get_comic_link(&self) -> String {
+        format!("{}/{}", XKCD_URL, self.num)
+    }
+
+    fn get_explain_link(&self) -> String {
+        format!("{}/{}", EXPLAIN_URL, self.num)
+    }
+
+    fn get_date(&self) -> LocalDate {
+        // this is cringe and should get cleaned up
+        LocalDate::ymd(
+            self.year.parse::<i64>().unwrap(),
+            Month::from_one(self.month.parse::<i8>().unwrap()).unwrap(),
+            self.day.parse::<i8>().unwrap(),
+        )
+        .unwrap()
+    }
+
+    async fn get(num: u32, client: &reqwest::Client) -> Result<Comic, Box<dyn std::error::Error>> {
+        Ok(client
+            .get(format!("{}/{}/info.0.json", XKCD_URL, num))
+            .send()
+            .await?
+            .json::<Comic>()
+            .await?)
+    }
 }
 
 #[tokio::main]
@@ -20,7 +69,6 @@ async fn main() {
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
                 // run when bot connects to discord
-
                 if let Ok(guild_id) = std::env::var("TEST_GUILD") {
                     // add commands to the guild as guild-specific commands to get them to propagate faster
                     // waiting for discord is cringe
@@ -30,7 +78,9 @@ async fn main() {
                     register_in_guild(ctx, &commands(), guild).await?;
                 };
 
-                Ok(Data {})
+                Ok(Data {
+                    client: reqwest::Client::new(),
+                })
             })
         })
         .options(poise::FrameworkOptions {
@@ -45,5 +95,14 @@ async fn main() {
 #[poise::command(slash_command)]
 async fn ping(ctx: Context<'_>) -> Result<(), Error> {
     ctx.say("Pong!").await?;
+    Ok(())
+}
+
+/// reeee
+#[poise::command(slash_command)]
+async fn debug_get(ctx: Context<'_>, num: u32) -> Result<(), Error> {
+    let comic = Comic::get(num, &ctx.data().client).await.unwrap();
+    ctx.say(format!("Got comic! Title: {}", comic.title))
+        .await?;
     Ok(())
 }
